68cbff81a783d131de6afc434feb67a3cd8e7ab2
Gheorghi <valentina161012@gmail.com>
2026-03-26 20:13:27 +0200

    TomCat App working btw

---
+++ tomCatApp/src/main/java/org/application/tomcatapp/TomCatAppApplication.java
@@ -0,0 +1,13 @@
+package org.application.tomcatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomCatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomCatAppApplication.class, args);
+    }
+
+}
---
+++ tomCatApp/src/test/java/org/application/tomcatapp/TomCatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.application.tomcatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomCatAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
