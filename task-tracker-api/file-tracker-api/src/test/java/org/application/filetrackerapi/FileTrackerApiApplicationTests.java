06ad1704a011df7311c48ff86547e9716ed38a4a
Gheorghi <valentina161012@gmail.com>
2026-03-28 22:10:16 +0200

    Task Tracker on gradle

---
+++ task-tracker-api/file-tracker-api/src/test/java/org/application/filetrackerapi/FileTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.application.filetrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
